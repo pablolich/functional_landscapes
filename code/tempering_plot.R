5f1b6dc490e287c1aa2bc22c50680a63a219918a
pablolich <plechon@ucm.es>
2022-10-18 09:49:16 -0500

    .

--- code/tempering_plot.R
+++ code/tempering_plot.R
@@ -1,1 +1,17 @@
 setwd("~/Desktop/functional_landscapes/code")
+library(tidyverse)
+library(viridis)
+
+data =read.csv('../data/tot_results.csv') %>% 
+  filter()
+
+ggplot(data, 
+       aes(x = t, 
+           y = ssq))+
+  geom_line(aes(group = chain,
+                color = log(1+T)))+
+  scale_color_viridis(option='turbo')+
+  scale_y_continuous(trans='log')
+
+
+ggsave('../data/tempering_plot_series.pdf', width = 6, height = 3)
